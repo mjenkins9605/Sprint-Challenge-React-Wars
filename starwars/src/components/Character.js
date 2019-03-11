c654a59d06ea39c52cad61ed84d19dbe89d0bc0f
Michael Jenkins <mjenkins9605@vidangel.com>
2019-03-10 23:33:38 -0400

    Styling and MVP

--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -5,21 +5,21 @@ import './StarWars.css';
 function Character(props) {
     return (
         <div className="char-card">
-            <h1 className="char-name">Name: {props.charData.name}</h1>
-            <p className="char-info">Created: {props.charData.created}</p>
-            <p className="char-info">Edited: {props.charData.editied}</p>
-            <p className="char-info">Eye Color: {props.charData.eye_color}</p>
-            <p className="char-info">Films: {props.charData.films}</p>
-            <p className="char-info">Hair Color: {props.charData.hair_color}</p>
-            <p className="char-info">Height: {props.charData.height}</p>
-            <p className="char-info">Homeworld: {props.charData.homeworld}</p>
-            <p className="char-info">Mass: {props.charData.mass}</p>
-            <p className="char-info">Skin Color: {props.charData.skin_color}</p>
-            <p className="char-info">Birth Year: {props.charData.birth_year}</p>
-            <p className="char-info">Species: {props.charData.species}</p>
-            <p className="char-info">Starships: {props.charData.starships}</p>
-            <p className="char-info">URL: {props.charData.url}</p>
-            <p className="char-info">Vehicles: {props.charData.vehicles}</p>
+            <h1 className="char-name"><strong>Name:</strong> {props.charData.name}</h1>
+            <p className="char-info"><strong>Created:</strong> {props.charData.created}</p>
+            <p className="char-info"><strong>Edited:</strong> {props.charData.editied}</p>
+            <p className="char-info"><strong>Eye Color:</strong> {props.charData.eye_color}</p>
+            <p className="char-info"><strong>Films:</strong> <a href={props.charData.films} target="_blank">Click Here</a></p>
+            <p className="char-info"><strong>Hair Color:</strong> {props.charData.hair_color}</p>
+            <p className="char-info"><strong>Height:</strong> {props.charData.height}</p>
+            <p className="char-info"><strong>Homeworld:</strong> <a href={props.charData.homeworld} target="_blank">Click Here</a></p>
+            <p className="char-info"><strong>Mass:</strong> {props.charData.mass}</p>
+            <p className="char-info"><strong>Skin Color:</strong> {props.charData.skin_color}</p>
+            <p className="char-info"><strong>Birth Year:</strong> {props.charData.birth_year}</p>
+            <p className="char-info"><strong>Species:</strong> <a href={props.charData.species} target="_blank">Click Here</a></p>
+            <p className="char-info"><strong>Starships:</strong> <a href={props.charData.starships} target="_blank">Click Here</a></p>
+            <p className="char-info"><strong>URL:</strong> <a href={props.charData.url} target="_blank">Click Here</a></p>
+            <p className="char-info"><strong>Vehicles:</strong> <a href={props.charData.vehicles} target="_blank">Click Here</a></p>
         </div>
     );
 }
